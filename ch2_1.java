1e2252765150bf5e372a09b06150432172bf5af3
TKU-FCP-412637141 <sunmimi28281130@gmail.com>
2024-07-02 11:56:16 +0800

    Add files via upload

---
+++ ch2_1.java
@@ -0,0 +1,14 @@
+import javax.print.DocFlavor.STRING;
+
+public class ch2_1 {//定義類別ch2_1
+    public static void main(String[]args){//main()函數，主程式開始
+        int num1=2;//宣告整數num
+        int num2=3;
+        float num3=4;
+        String num4="五";
+        System.out.println(num1+"cats are running");//印出字串及變數內容
+        System.out.println(num2+"dogs are running");
+        System.out.println(num3+"birds are running");
+        System.out.println(num4+"ducks are running");
+    }
+}
