f577e61f78f1c074e6c1bb13ceabd560a0beaaeb
TKU-FCP-412637141 <sunmimi28281130@gmail.com>
2024-07-03 10:59:16 +0800

    新增ch_2,3

--- ch2_1.java
+++ ch2_1.java
@@ -2,8 +2,9 @@ import javax.print.DocFlavor.STRING;
 
 public class ch2_1 {//定義類別ch2_1
     public static void main(String[]args){//main()函數，主程式開始
-        int num1=2;//宣告整數num
-        int num2=3;
+        int num1,num2;//宣告變數 num1,num為正整數
+        num1=1; //給值
+        num2=2; //給值
         float num3=4;
         String num4="五";
         System.out.println(num1+"cats are running");//印出字串及變數內容
