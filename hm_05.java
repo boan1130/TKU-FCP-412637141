737403b048a281ca966df43c57be719afec4773f
TKU-FCP-412637141 <sunmimi28281130@gmail.com>
2024-07-03 14:29:08 +0800

    新增hm_05

--- hm_05.java
+++ hm_05.java
@@ -1,5 +1,5 @@
-public class hm_05 {
-    static void main(String[]args){
+public class hm_05 { //定義類別Ch_2
+    public static void main (String[]args){
         int a=12;
         int b=7;
         System.out.println(a+b);
